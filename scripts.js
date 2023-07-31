da6210e5a216fe99c13f9dbb4263282afd2d69e3
khomotso gojela <motsoak49@gmail.com>
2023-07-31 10:26:33 +0200

    disabling button if max/min reached

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const MAX_NUMBER = 15
-const MIN_NUMBETR = -5
+const MIN_NUMBER = -5
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
@@ -8,11 +8,26 @@ const add = document.querySelector('[data-key="add"]')
 const subtractHandler = () => {
     const newValue = parseInt(number.value) - 1
     number.value = newValue
+
+    if (number.value <= MAX_NUMBER) {
+        add.disabled = false
+    }
+
+    if (number.value <= MIN_NUMBER) {
+        subtract.disabled = true
+    }
 }
 
 const addHandler = () => {
     const newValue = parseInt(number.value) + 1
     number.value = newValue
+    if (number.value >= MIN_NUMBER) {
+        subtract.disabled = false
+    }
+
+    if (number.value >= MAX_NUMBER) {
+        add.disabled = true
+    }
 }
 
 subtract.addEventListener('click',subtractHandler)
